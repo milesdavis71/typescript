223d2b00b87e567ea420cdf47be2b9a6bd395da3
milesdavis71 <hevesiistvan71@hotmail.com>
2021-09-11 18:24:11 +0200

    3. fejezet kész.

--- 03/src/analytics.ts
+++
@@ -1,5 +0,0 @@
-function sendAnalytics(data: string) {
-  console.log(data);
-  logged = true; // bool
-  console.log(logged); // string
-}
--- dist/app.js
+++ dist/app.js
@@ -1,5 +1,4 @@
 "use strict";
-<<<<<<< HEAD
 window.addEventListener('load', function () {
     const counterContainer = document.querySelector('.count');
     let counter = 0;
@@ -11,19 +10,11 @@ window.addEventListener('load', function () {
         }
         counter++;
         if (counterContainer) {
-            counterContainer.insertAdjacentHTML('beforeend', `<p>${counter}</p>`);
+            counterContainer.insertAdjacentHTML("beforeend", `<p>${counter}</p>`);
         }
     });
 });
 // btn?.addEventListener('click', function (event) {
 //   console.log(event.type);
 // });
-=======
-const myself = 'pitju';
-myself;
-const button = document.querySelector('button');
-<<<<<<< HEAD
-=======
-button === null || button === void 0 ? void 0 : button.addEventListener;
->>>>>>> 72451f13009285222964b24aafab662ef659fe99
->>>>>>> 39b77ed04a2f96a35863388ba1079b32c60a1ce7
+//# sourceMappingURL=app.js.map
