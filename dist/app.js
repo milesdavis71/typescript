a15cff28cd026b9bcdab51b336058318dfafca95
milesdavis71 <hevesiistvan71@hotmail.com>
2021-09-12 20:02:38 +0200

    add infynite value with rest pattern

--- dist/app.js
+++ dist/app.js
@@ -1,6 +1,17 @@
 "use strict";
-const add = (a, b = 7) => a + b;
+// const add = (a: number, b: number = 7) => a + b;
 // const printOutput = (output: string | number) => console.log(output);
 const printOutput = output => console.log(output);
-printOutput(add(5));
+// printOutput(add(5));
+const sport = ['foci', 'hoki', 'sí'];
+const sportTobb = ['darts', 'teke'];
+const [a, b, c, ...others] = [...sport, ...sportTobb];
+console.log(a, b, c, others);
+const add = (...numbers) => {
+    return numbers.reduce((curResult, curValue) => {
+        return curResult + curValue;
+    }, 0);
+};
+const addedNumbers = add(5, 10, 2, 3.7);
+console.log(addedNumbers);
 //# sourceMappingURL=app.js.map
--- src/app.ts
+++ src/app.ts
@@ -1,7 +1,21 @@
-const add = (a: number, b: number = 7) => a + b;
+// const add = (a: number, b: number = 7) => a + b;
 
 // const printOutput = (output: string | number) => console.log(output);
 
 const printOutput: (a: string | number) => void = output => console.log(output);
 
-printOutput(add(5));
+// printOutput(add(5));
+
+const sport = ['foci', 'hoki', 'sí'];
+const sportTobb = ['darts', 'teke'];
+const [a, b, c, ...others] = [...sport, ...sportTobb];
+console.log(a, b, c, others);
+
+const add = (...numbers: number[]) => {
+  return numbers.reduce((curResult, curValue) => {
+    return curResult + curValue;
+  }, 0);
+};
+
+const addedNumbers = add(5, 10, 2, 3.7);
+console.log(addedNumbers);
