3ad70658f2b251671009a54ec031324e6c52fca5
milesdavis71 <hevesiistvan71@hotmail.com>
2021-09-16 17:30:15 +0200

    05. Classes & Interfaces fejezet – inicializálás.

--- src/app.ts
+++ src/app.ts
@@ -1,10 +1,11 @@
 class Department {
   name: string;
-
   constructor(n: string) {
     this.name = n;
   }
+  describe() {
+    console.log('Department ' + this.name);
+  }
 }
-
 const accounting = new Department('Accounting');
-console.log(accounting);
+accounting.describe();
