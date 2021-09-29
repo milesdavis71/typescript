e7c99db2419b6eef51465c986c25e8c96e260e95
milesdavis71 <hevesiistvan71@hotmail.com>
2021-09-29 17:55:54 +0200

    PC - Inheritance folytatás - utolsó osztályok

--- dist/app.js
+++ dist/app.js
@@ -13,15 +13,34 @@ class Department {
         this.employees.push(employee);
     }
     printEmployeeInformation() {
-        console.log(`Munkavállalók száma: ${this.employees.length}`);
+        // console.log(`Munkavállalók száma: ${this.employees.length}`);
         console.log(this.employees);
     }
 }
-const accounting = new Department('d1', 'Accounting');
-accounting.addEmployee('pityu');
-accounting.addEmployee('gabi');
-accounting.name = 'Huhu';
-accounting.printEmployeeInformation();
+class ITDepartment extends Department {
+    constructor(id, admins) {
+        super(id, 'IT');
+        this.admins = admins;
+    }
+}
+const it = new ITDepartment('d2', ['max']);
+it.addEmployee('pityu');
+it.addEmployee('gabi');
+it.describe();
+it.printEmployeeInformation();
+console.log(it);
 // const accountingCopy = { name: 'DUMMY', describe: accounting.describe };
 // accountingCopy.describe();
+class AccountingDepartment extends Department {
+    constructor(id, reports) {
+        super(id, 'IT');
+        this.reports = reports;
+    }
+    addReport(text) {
+        this.reports.push(text);
+    }
+    printReports() {
+        console.log(this.reports);
+    }
+}
 //# sourceMappingURL=app.js.map
--- src/app.ts
+++ src/app.ts
@@ -12,21 +12,39 @@ class Department {
   }
 
   printEmployeeInformation() {
-    console.log(`Munkavállalók száma: ${this.employees.length}`);
+    // console.log(`Munkavállalók száma: ${this.employees.length}`);
     console.log(this.employees);
   }
 }
 
 class ITDepartment extends Department {
-  constructor(id: string) {
-    super('id', 'IT');
+  admins: string[];
+  constructor(id: string, admins: string[]) {
+    super(id, 'IT');
+    this.admins = admins;
   }
 }
-const accounting = new ITDepartment('d2');
+const it = new ITDepartment('d2', ['max']);
 
-accounting.addEmployee('pityu');
-accounting.addEmployee('gabi');
-accounting.name = 'Huhu';
-accounting.printEmployeeInformation();
+it.addEmployee('pityu');
+it.addEmployee('gabi');
+
+it.describe();
+it.printEmployeeInformation();
+console.log(it);
 // const accountingCopy = { name: 'DUMMY', describe: accounting.describe };
 // accountingCopy.describe();
+
+class AccountingDepartment extends Department {
+  constructor(id: string, private reports: string[]) {
+    super(id, 'IT');
+  }
+
+  addReport(text: string) {
+    this.reports.push(text);
+  }
+
+  printReports() {
+    console.log(this.reports);
+  }
+}
