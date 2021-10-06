5d15971737840d2bceef3dae98c31f8fa90d26e8
milesdavis71 <OttosMops71>
2021-10-06 17:03:01 +0200

    More on Type Guards kész.

--- dist/app.js
+++ dist/app.js
@@ -1,14 +1,25 @@
 "use strict";
-class Person {
-    constructor(n) {
-        this.age = 30;
-        this.name = n;
+const e1 = {
+    name: 'Max',
+    privileges: ['create server'],
+    startDate: new Date(),
+};
+function add(a, b) {
+    if (typeof a === 'string' || typeof b === 'string') {
+        return a.toString() + b.toString();
     }
-    greet(phrase) {
-        console.log(`${phrase} ${this.name}`);
+    return a + b;
+}
+function printEmployeeInformation(emp) {
+    console.log('Name: ' + emp.name);
+    if ('privileges' in emp) {
+        console.log('Privileges: ' + emp.privileges);
+    }
+    if ('startDate' in emp) {
+        console.log('Start date: ' + emp.startDate);
     }
 }
-let user1;
-user1 = new Person('Pitju');
-user1.greet('Hellllóóó');
+printEmployeeInformation(e1);
+printEmployeeInformation({ name: 'Atti', startDate: new Date() });
+printEmployeeInformation({ name: 'Márk', privileges: ['höhö'] });
 //# sourceMappingURL=app.js.map
---
+++ src/app.ts
@@ -0,0 +1,77 @@
+type Admin = {
+  name: string;
+  privileges: string[];
+};
+
+type Employee = {
+  name: string;
+  startDate: Date;
+};
+
+type ElevatedEmployee = Admin & Employee;
+
+const e1: ElevatedEmployee = {
+  name: 'Max',
+  privileges: ['create server'],
+  startDate: new Date(),
+};
+
+type Combinable = string | number;
+type Numeric = number | boolean;
+
+type Universal = Combinable & Number;
+
+function add(a: Combinable, b: Combinable) {
+  if (typeof a === 'string' || typeof b === 'string') {
+    return a.toString() + b.toString();
+  }
+  return a + b;
+}
+type UnknownEmployee = Employee | Admin;
+
+function printEmployeeInformation(emp: UnknownEmployee) {
+  console.log('Name: ' + emp.name);
+  if ('privileges' in emp) {
+    console.log('Privileges: ' + emp.privileges);
+  }
+  if ('startDate' in emp) {
+    console.log('Start date: ' + emp.startDate);
+  }
+}
+
+printEmployeeInformation(e1);
+printEmployeeInformation({ name: 'Atti', startDate: new Date() });
+printEmployeeInformation({ name: 'Márk', privileges: ['höhö'] });
+
+class Car {
+  drive() {
+    console.log('Driving...');
+  }
+}
+
+class Truck {
+  drive() {
+    console.log('Driving a truck...');
+  }
+  loadCargo(amount: number) {
+    console.log(`Loading cargo ... ${amount}`);
+  }
+}
+
+type Vehicle = Car | Truck;
+
+const v1 = new Car();
+const v2 = new Truck();
+
+function useVehicle(vehicle: Vehicle) {
+  vehicle.drive();
+  // if ('loadCargo' in vehicle) {
+  //   vehicle.loadCargo(1000);
+  // }
+  if (vehicle instanceof Truck) {
+    vehicle.loadCargo(1000);
+  }
+}
+
+useVehicle(v1);
+useVehicle(v2);
