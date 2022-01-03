21e1da10a272c5ed93df342541f84a5736972720
istvan <hevesiistvan71@hotmail.com>
2022-01-03 20:02:38 +0100

    drag and drop gyakorlás 2.

--- src/app.ts
+++ src/app.ts
@@ -1,10 +1,55 @@
-
+enum ProjectStatus {
+  Active,
+  Finished,
+}
 
 class Project {
-  constructor(parameters) {
-    
+  constructor(
+    public id: string,
+    public title: string,
+    public description: string,
+    public people: number,
+    public status: ProjectStatus
+  ) {}
+}
+
+type Listener<T> = (items: T[]) => void;
+
+class State<T> {
+  listeners: Listener<T>[] = [];
+  addListener(listenerFn: Listener<T>) {
+    this.listeners.push(listenerFn);
   }
 }
+
+class ProjectState extends State<Project> {
+  private projects: Project[] = [];
+  private static instance: ProjectState;
+  private constructor() {
+    super();
+  }
+  static getInstance() {
+    if (this.instance) {
+      return this.instance;
+    }
+    this.instance = new ProjectState();
+    return this.instance;
+  }
+
+  addProjects(title: string, desc: string, numOfPeople: number) {
+    const newProject = new Project(
+      Math.random.toString(),
+      title,
+      desc,
+      numOfPeople,
+      ProjectStatus.Active
+    );
+    this.projects.push(newProject);
+    this.listeners.map(listenerFn => listenerFn(this.projects.slice()));
+  }
+}
+
+const prjStateInstance = ProjectState.getInstance();
 
 abstract class Component<T extends HTMLElement, U extends HTMLElement> {
   templateElement: HTMLTemplateElement;
