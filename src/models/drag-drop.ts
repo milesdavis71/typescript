b0d0a934751cc6886f4a17a96cd2cc4c4b0a1cec
istvan <hevesiistvan71@hotmail.com>
2022-02-01 19:58:01 +0100

    fájlok szétszedése folyamatban.

--- src/models/drag-drop.ts
+++ src/models/drag-drop.ts
@@ -1,9 +1,8 @@
-//  Drag &Drop interfaces
-interface Draggable {
+export interface Draggable {
   dragStartHandler(event: DragEvent): void;
   dragEndHandler(event: DragEvent): void;
 }
-interface DragTarget {
+export interface DragTarget {
   dragOverHandler(event: DragEvent): void;
   dropHandler(event: DragEvent): void;
   dragLeaveHandler(event: DragEvent): void;
