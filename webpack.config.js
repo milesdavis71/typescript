cb18274557bd56b0d03e93a048763b33e296b713
istvan <hevesiistvan71@hotmail.com>
2022-02-12 06:51:10 +0100

    6. Finishing the Setup & Adding webpack-dev-server KÉSZ

--- webpack.config.js
+++ webpack.config.js
@@ -6,10 +6,19 @@ module.exports = {
   output: {
     filename: 'bundle.js',
     path: path.resolve(__dirname, 'dist'),
+    publicPath: 'dist',
   },
   devtool: 'inline-source-map',
   module: {
-    rules: [{ test: /\.ts$/, use: 'ts-loader', exclude: /node_modules/ }],
+    rules: [
+      {
+        test: /\.ts$/,
+        use: 'ts-loader',
+        exclude: /node_modules/,
+      },
+    ],
   },
-  resolve: { extensions: ['.ts', '.js'] },
+  resolve: {
+    extensions: ['.ts', '.js'],
+  },
 };
